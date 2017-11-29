5193e83153074e45dee5686f3546ace67f647a62
SchwiftyDoc <cs1first2>
2017-11-29 08:09:25 +0000

    Correction: useMongoClient

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'),
     app = express(),
+    bind = '0.0.0.0',
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
     User = require('./api/models/userModel'),
@@ -15,6 +16,6 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/userRoutes');
 routes(app);
 
-app.listen(port);
+app.listen(port, bind);
 
 console.log('User RESTful API server started on: ' + port);
