b4037bc4afbb7aa8e4e43cbd655846931e502dc5
SchwiftyDoc <cs1first2>
2017-11-29 07:50:08 +0000

    Server.js end

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    User = require('./api/models/userModel'),
+    bodyParser = require('body-parser');
+
+// Mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Userdb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/userRoutes');
+routes(app);
 
 app.listen(port);
 
